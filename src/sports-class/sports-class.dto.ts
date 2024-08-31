b65aa170c1fa815aa05acaa8accef329e68b0f45
Lovre Gusar <lovreg79@gmail.com>
2024-08-31 18:12:06 +0200

    docs: add swagger

--- src/sports-class/sports-class.dto.ts
+++ src/sports-class/sports-class.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SportsClassDto {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   weekSchedule: string;
+  @ApiProperty()
   classDuration: string;
+  @ApiProperty()
   description: string;
 }
