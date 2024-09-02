16e9cfc80a17ddbab11e2543e3b99bfd8537707d
Lovre Gusar <lovreg79@gmail.com>
2024-09-02 13:04:19 +0200

    fix: add password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../user/user.entity';
 import { Role } from '../role/role.enum';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,8 +14,8 @@ export class AuthService {
 
   async signIn(email: string, pass: string): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(email);
-    // TODO: implement password hashing
-    const hash = pass;
+    const hash = await bcrypt.hash(pass, user.salt);
+
     if (user?.passwordHash !== hash) {
       throw new UnauthorizedException();
     }
@@ -22,11 +23,12 @@ export class AuthService {
   }
 
   async signUp(email: string, pass: string): Promise<{ access_token: string }> {
+    const { hash, salt } = await this.hashAndGenerateSalt(pass);
     let user: User = {
       email: email,
-      // TODO: implement password hashing
-      passwordHash: pass,
-      salt: pass,
+      passwordHash: hash,
+      salt: salt,
+      // TODO: role shouldn't get hardcoded like this, there is no way to make an admin user
       role: Role.User,
     };
     user = await this.usersService.save(user);
@@ -37,6 +39,19 @@ export class AuthService {
     const payload = { sub: user.id, email: user.email, role: user.role };
     return {
       access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async hashAndGenerateSalt(
+    password: string,
+  ): Promise<{ hash: string; salt: string }> {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+
+    return {
+      hash,
+      salt,
     };
   }
 }
